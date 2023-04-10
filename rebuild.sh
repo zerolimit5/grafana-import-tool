7235f8f20400b032f5d94bc8bd21277952c056eb
zerolimit5 <jescudero@ea.com>
2023-04-10 19:21:26 -0400

    sync

---
+++ rebuild.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rm -rf /usr/local/lib/python3.9/site-packages/grafana_import /Users/jescudero/.pyenv/versions/grafana-tool/lib/python3.9/site-packages/grafana_import2-0.2.2-py3.9.egg
+
+pip3 install  .
